258f89c301d5851d8d664070fde4acfd43ef1c8b
DougRzz <doug.redden@gmail.com>
2018-10-14 00:05:10 +0100

    Import platform

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup, find_packages, Command
 from setuptools.command.sdist import sdist
 from setuptools.command.build_py import build_py
 from setuptools.command.egg_info import egg_info
+import platform
+
 
 from subprocess import check_call
 
