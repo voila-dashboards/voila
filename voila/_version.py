f74f66fdca3b11186f208b43fea238de2941a7dc
Sylvain Corlay <sylvain.corlay@gmail.com>
2018-10-17 15:06:16 +0200

    Release 0.0.3

--- voila/_version.py
+++ voila/_version.py
@@ -6,5 +6,5 @@
 # The full license is in the file LICENSE, distributed with this software.  #
 #############################################################################
 
-version_info = (0, 0, 2)
+version_info = (0, 0, 3)
 __version__ = '.'.join(map(str, version_info))
