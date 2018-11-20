2b331e40af6fe6a6c6b408a6bb4fc41c275f7e13
Sylvain Corlay <sylvain.corlay@gmail.com>
2018-11-20 14:00:35 +0100

    Simplify render widgets

--- js/WidgetApplication.js
+++ js/WidgetApplication.js
@@ -31,36 +31,9 @@ export class WidgetApplication {
         this._kernel = kernel;
 
         const manager = new WidgetManager(kernel, this._loader);
-
-        const options = {
-            msgType: 'custom_message',
-            channel: 'shell'
-        }
-
-        const msg = KernelMessage.createShellMessage(options)
-        const execution = kernel.sendShellMessage(msg, true)
-        execution.onIOPub = (msg) => {
-            // If we have a display message, display the widget.
-            if (KernelMessage.isDisplayDataMsg(msg)) {
-                let widgetData = msg.content.data['application/vnd.jupyter.widget-view+json'];
-                if (widgetData !== undefined && widgetData.version_major === 2) {
-                    let model = manager.get_model(widgetData.model_id);
-                    if (model !== undefined) {
-                        model.then(model => {
-                            manager.display_model(msg, model);
-                        });
-                    }
-                }
-            }
-            else if (KernelMessage.isErrorMsg(msg)) {
-                // Show errors to help with debugging
-                errorView.showError(msg.content)
-            }
-        }
     }
 
     async cleanWidgets() {
-        // terminate kernel process
         this._kernel.shutdown()
     }
 }
