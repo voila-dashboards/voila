fffe556567cb96c6fe79215fff9e6b8c4b7a63bd
Maarten Breddels <maartenbreddels@gmail.com>
2019-03-14 19:42:19 +0100

    test using voila as instance

---
+++ tests/template_cli_test.py
@@ -0,0 +1,20 @@
+# tests programmatic config of template sytem
+import pytest
+import os
+
+BASE_DIR = os.path.dirname(__file__)
+
+
+@pytest.fixture
+def voila_args_extra():
+    path_gridstack = os.path.abspath(os.path.join(BASE_DIR, '../share/jupyter/voila/template/gridstack/nbconvert_templates'))
+    path_default = os.path.abspath(os.path.join(BASE_DIR, '../share/jupyter/voila/template/default/nbconvert_templates'))
+    return ['--template=None', '--Voila.nbconvert_template_paths=[%r, %r]' % (path_gridstack, path_default)] 
+
+
+@pytest.mark.gen_test
+def test_template_gridstack(http_client, base_url):
+    response = yield http_client.fetch(base_url)
+    assert response.code == 200
+    assert 'gridstack.css' in response.body.decode('utf-8')
+    assert 'Hi Voila' in response.body.decode('utf-8')
