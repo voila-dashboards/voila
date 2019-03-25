d590e7ae26a72fe77c5bbdbd662febcffc2ee624
Maarten Breddels <maartenbreddels@gmail.com>
2019-03-25 15:25:46 +0100

    refactored a bit, made it the default template

--- share/jupyter/voila/template/default/nbconvert_templates/voila.tpl
+++ share/jupyter/voila/template/default/nbconvert_templates/voila.tpl
@@ -1,64 +1,76 @@
+
 {%- extends 'base.tpl' -%}
-{% from 'mathjax.tpl' import mathjax %}
 
-{%- block html_head_css -%}
+{%- block header -%}
+{%- endblock header -%}
 
+{% block body %}
+    <script id="jupyter-config-data" type="application/json">
+    {
+        "baseUrl": "{{server_root}}",
+        "kernelId": "{{resources.kernel_id}}"
+    }
+    </script>
+    <script type="text/javascript">
+    // remove the loading element
+    (function() {
+      var el = document.getElementById("loading")
+      el.parentNode.removeChild(el)
+    })()
+    </script>
 {% for css in resources.inlining.css -%}
     <style type="text/css">
     {{ css }}
     </style>
 {% endfor %}
 
-<style type="text/css">
+    <style type="text/css">
 
-/* Overrides of notebook CSS for static HTML export */
-body {
-  overflow: visible;
-  padding: 8px;
-}
+    /* Overrides of notebook CSS for static HTML export */
+    body {
+      overflow: visible;
+      padding: 8px;
+    }
 
-div#notebook {
-  overflow: visible;
-  border-top: none;
-  padding: 20px;
-}
+    div#notebook {
+      overflow: visible;
+      border-top: none;
+      padding: 20px;
+    }
 
-{%- if resources.global_content_filter.no_prompt-%}
-div#notebook-container{
-  padding: 6ex 12ex 8ex 12ex;
-}
-{%- endif -%}
+    {%- if resources.global_content_filter.no_prompt-%}
+    div#notebook-container{
+      padding: 6ex 12ex 8ex 12ex;
+    }
+    {%- endif -%}
 
-@media print {
-  div.cell {
-    display: block;
-    page-break-inside: avoid;
-  }
-  div.output_wrapper {
-    display: block;
-    page-break-inside: avoid;
-  }
-  div.output {
-    display: block;
-    page-break-inside: avoid;
-  }
-}
-</style>
+    @media print {
+      div.cell {
+        display: block;
+        page-break-inside: avoid;
+      }
+      div.output_wrapper {
+        display: block;
+        page-break-inside: avoid;
+      }
+      div.output {
+        display: block;
+        page-break-inside: avoid;
+      }
+    }
+    </style>
 
-<!-- Custom stylesheet, it must be in the same directory as the html file -->
-<!-- <link rel="stylesheet" href="custom.css"> -->
 
-<!-- Loading mathjax macro -->
-{{ mathjax() }}
-{%- endblock html_head_css -%}
-
-{% block body %}
-<body>
-  <div tabindex="-1" id="notebook" class="border-box-sizing">
-    <div class="container" id="notebook-container">
-{{ super() }}
+    <div tabindex="-1" id="notebook" class="border-box-sizing">
+        <div class="container" id="notebook-container">
+            {{ super() }}
+        </div>
     </div>
-  </div>
 </body>
+
 {%- endblock body %}
+
+
+
+
 
--- voila/handler.py
+++ voila/handler.py
@@ -17,12 +17,20 @@ import jinja2
 
 
 class VoilaHandler(JupyterHandler):
-
     def initialize(self, **kwargs):
-        self.notebook_path = kwargs.pop('notebook_path', [])    # should it be []
+        self.notebook_path = kwargs.pop('notebook_path', [])  # should it be []
         self.strip_sources = kwargs.pop('strip_sources', True)
         self.nbconvert_template_paths = kwargs.pop('nbconvert_template_paths', [])
         self.exporter_config = kwargs.pop('config', None)
+        # generate a list of nbextensions that are enabled for the classical notebook
+        # a template can use that to load classical notebook extensions, but does not have to
+        notebook_config = self.config_manager.get('notebook')
+        # except for the widget extension itself, since voila has its own
+        load_extensions = notebook_config.get('load_extensions', {})
+        if "jupyter-js-widgets/extension" in load_extensions:
+            load_extensions["jupyter-js-widgets/extension"] = False
+        self.nbextensions = [name for name, enabled in load_extensions.items() if enabled]
+
 
     @tornado.web.authenticated
     @tornado.gen.coroutine
@@ -31,15 +39,6 @@ class VoilaHandler(JupyterHandler):
             path += '.ipynb'  # when used as a jupyter server extension, we don't use the extension
         # if the handler got a notebook_path argument, always serve that
         notebook_path = self.notebook_path or path
-
-        # generate a list of nbextensions that are enabled for the classical notebook
-        # a template can use that to load classical notebook extensions, but does not have to
-        notebook_config = self.config_manager.get('notebook')
-        # except for the widget extension itself, since voila has its own
-        load_extensions = notebook_config.get('load_extensions', {})
-        if "jupyter-js-widgets/extension" in load_extensions:
-            load_extensions["jupyter-js-widgets/extension"] = False
-        nbextensions = [name for name, enabled in load_extensions.items() if enabled]
 
         model = self.contents_manager.get(path=notebook_path)
         if 'content' in model:
@@ -53,24 +52,32 @@ class VoilaHandler(JupyterHandler):
 
         resources = {}  # we may want to have resources similar to nbconvert for consistency
         resources['metadata'] = {'name': notebook_path}
+        html_template_namespace = dict(
+            page_title=notebook.metadata.get('title', '') or resources['metadata']['name'],
+            server_root=self.settings['server_root_dir'],
+            nbextensions=self.nbextensions,
+            path=path or '',
+            nb=notebook,
+            resources=resources,
+        )
+        self.render_fragment('voila_header', **html_template_namespace)
+        self.render_fragment_body(notebook)
+        self.render_fragment('voila_footer', **html_template_namespace)
+
+    def render_fragment(self, name, **html_template_namespace):
         template = None
         try:
-            template = self.get_template('voila.html')
+            template = self.get_template(name + '.html')
         except jinja2.TemplateNotFound:
-            pass  # voila.html is optional
+            pass  # a <fragment name>.html is optional
         else:
-            html = self.render_template(template,
-                                        page_title='test',
-                                        server_root=self.settings['server_root_dir'],
-                                        nbextensions=nbextensions,
-                                        path=path or '',
-                                        nb=notebook,
-                                        resources=resources
-                                        )
+            html = self.render_template(template, **html_template_namespace)
             self.set_header('Content-Type', 'text/html')
             self.write(html)
             self.flush()  # next phase, flush html to client so it can render (progressive)
 
+    @tornado.gen.coroutine
+    def render_fragment_body(self, notebook):
         # Fetch kernel name from the notebook metadata
         kernel_name = notebook.metadata.get('kernelspec', {}).get('name', self.kernel_manager.default_kernel_name)
 
@@ -80,16 +87,10 @@ class VoilaHandler(JupyterHandler):
         result = executenb(notebook, km=km)
 
         # render notebook to html
-        resources = {
-            'kernel_id': kernel_id,
-            'base_url': self.base_url,
-            'nbextensions': nbextensions
-        }
+        resources = {'kernel_id': kernel_id, 'base_url': self.base_url, 'nbextensions': self.nbextensions}
 
         exporter = HTMLExporter(
-            template_file='voila.tpl',
-            template_path=self.nbconvert_template_paths,
-            config=self.exporter_config
+            template_file='voila.tpl', template_path=self.nbconvert_template_paths, config=self.exporter_config
         )
 
         if self.strip_sources:
@@ -100,10 +101,13 @@ class VoilaHandler(JupyterHandler):
         # Filtering out empty cells.
         def filter_empty_code_cells(cell):
             return (
-                cell.cell_type != 'code' or                     # keep non-code cells
-                (cell.outputs and not exporter.exclude_output)  # keep cell if output not excluded and not empty
-                or not exporter.exclude_input                   # keep cell if input not excluded
+                cell.cell_type != 'code'
+                or (  # keep non-code cells
+                    cell.outputs and not exporter.exclude_output
+                )  # keep cell if output not excluded and not empty
+                or not exporter.exclude_input  # keep cell if input not excluded
             )
+
         result.cells = list(filter(filter_empty_code_cells, result.cells))
 
         html, resources = exporter.from_notebook_node(result, resources=resources)
