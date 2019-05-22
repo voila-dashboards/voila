41f84012513b2f28716f3db4469fdeff888b71ad
Maarten Breddels <maartenbreddels@gmail.com>
2019-05-22 15:10:00 +0200

    test(execute,strip_sources): test if sources are really stripped, or not when configured

--- tests/server/no_strip_sources_test.py
+++ tests/server/no_strip_sources_test.py
@@ -1,14 +1,14 @@
-# test basics of voila running a notebook
 import pytest
-import tornado.web
-import tornado.gen
-import re
 import json
+import os
 
-try:
-    from unittest import mock
-except:
-    import mock
+
+BASE_DIR = os.path.dirname(__file__)
+
+
+@pytest.fixture
+def jupyter_server_args_extra():
+    return ['--VoilaConfiguration.strip_sources=False']
 
 
 @pytest.mark.gen_test
@@ -17,5 +17,6 @@ def test_hello_world(http_client, print_notebook_url):
     assert response.code == 200
     html_text = response.body.decode('utf-8')
     assert 'Hi Voila' in html_text
-    assert 'print' not in html_text, 'by default the source code should be stripped'
+    assert 'print' in html_text, 'the source code should *NOT* be stripped'
     assert 'gridstack.css' not in html_text, "gridstack should not be the default"
+
