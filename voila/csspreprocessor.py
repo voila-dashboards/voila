6152cb3913e4a443f80db845a36e002c8151281c
Sylvain Corlay <sylvain.corlay@gmail.com>
2019-05-22 11:08:22 +0200

    Add copyright headers

--- voila/csspreprocessor.py
+++ voila/csspreprocessor.py
@@ -1,5 +1,10 @@
-"""Module that pre-processes the notebook for export to HTML.
-"""
+#############################################################################
+# Copyright (c) 2018, Voila Contributors                                    #
+#                                                                           #
+# Distributed under the terms of the BSD 3-Clause License.                  #
+#                                                                           #
+# The full license is in the file LICENSE, distributed with this software.  #
+#############################################################################
 
 import hashlib
 
