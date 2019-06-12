ad8cfe104ab4acba906db26adcac80c4b4013fbc
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2019-06-12 10:57:13 +0200

    Add prettier to auto-format on commit
    Follow the same style and workflow as used in the main JupyterLab repo.

--- packages/jupyterlab-voila/src/index.ts
+++ packages/jupyterlab-voila/src/index.ts
@@ -1,67 +1,59 @@
-import {
-  JupyterLab, JupyterLabPlugin
-} from '@jupyterlab/application';
+import { JupyterLab, JupyterLabPlugin } from "@jupyterlab/application";
 
 import {
-  INotebookTracker, NotebookPanel, INotebookModel
-} from '@jupyterlab/notebook';
+  INotebookTracker,
+  NotebookPanel,
+  INotebookModel
+} from "@jupyterlab/notebook";
 
-import {
-  ReadonlyJSONObject
-} from '@phosphor/coreutils';
+import { ReadonlyJSONObject } from "@phosphor/coreutils";
 
-import {
-    ICommandPalette, MainAreaWidget, IFrame,
-} from '@jupyterlab/apputils';
+import { ICommandPalette, MainAreaWidget, IFrame } from "@jupyterlab/apputils";
 
-import {
-  IMainMenu
-} from '@jupyterlab/mainmenu';
+import { IMainMenu } from "@jupyterlab/mainmenu";
 
-import {
-  PageConfig, PathExt
-} from '@jupyterlab/coreutils';	
+import { PageConfig, PathExt } from "@jupyterlab/coreutils";
 
-import {
-  ToolbarButton
-} from '@jupyterlab/apputils';
+import { ToolbarButton } from "@jupyterlab/apputils";
 
 import { DocumentRegistry } from "@jupyterlab/docregistry";
 
-import { IDisposable } from '@phosphor/disposable';
+import { IDisposable } from "@phosphor/disposable";
 
-import '../style/index.css';
+import "../style/index.css";
 
 export namespace CommandIDs {
+  export const voilaRender = "notebook:render-with-voila";
 
-    export const voilaRender = 'notebook:render-with-voila';
-
-    export const voilaOpen = "notebook:open-with-voila";
+  export const voilaOpen = "notebook:open-with-voila";
 }
 
-const VOILA_ICON_CLASS = 'jp-MaterialIcon jp-VoilaIcon';
+const VOILA_ICON_CLASS = "jp-MaterialIcon jp-VoilaIcon";
 
-class VoilaRenderButton implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel> {
-
+class VoilaRenderButton
+  implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel> {
   constructor(app: JupyterLab) {
     this.app = app;
   }
 
   readonly app: JupyterLab;
 
-  createNew(panel: NotebookPanel, context: DocumentRegistry.IContext<INotebookModel>): IDisposable {
+  createNew(
+    panel: NotebookPanel,
+    context: DocumentRegistry.IContext<INotebookModel>
+  ): IDisposable {
     let renderVoila = () => {
-      this.app.commands.execute('notebook:render-with-voila');
+      this.app.commands.execute("notebook:render-with-voila");
     };
 
     let button = new ToolbarButton({
-      className: 'voilaRender',
+      className: "voilaRender",
       iconClassName: VOILA_ICON_CLASS,
       onClick: renderVoila,
-      tooltip: 'Render with Voila'
+      tooltip: "Render with Voila"
     });
 
-    panel.toolbar.insertItem(9, 'voilaRender', button);
+    panel.toolbar.insertItem(9, "voilaRender", button);
 
     return button;
   }
@@ -71,28 +63,32 @@ class VoilaRenderButton implements DocumentRegistry.IWidgetExtension<NotebookPan
  * Initialization data for the jupyterlab-voila extension.
  */
 const extension: JupyterLabPlugin<void> = {
-  id: 'jupyterlab-voila',
+  id: "jupyterlab-voila",
   autoStart: true,
   requires: [INotebookTracker],
   optional: [ICommandPalette, IMainMenu],
-  activate: (app: JupyterLab, notebooks: INotebookTracker, palette: ICommandPalette, menu: IMainMenu | null) => {
-
+  activate: (
+    app: JupyterLab,
+    notebooks: INotebookTracker,
+    palette: ICommandPalette,
+    menu: IMainMenu | null
+  ) => {
     function getCurrent(args: ReadonlyJSONObject): NotebookPanel | null {
-        const widget = notebooks.currentWidget;
-        const activate = args['activate'] !== false;
+      const widget = notebooks.currentWidget;
+      const activate = args["activate"] !== false;
 
-        if (activate && widget) {
-          app.shell.activateById(widget.id);
-        }
+      if (activate && widget) {
+        app.shell.activateById(widget.id);
+      }
 
-        return widget;
+      return widget;
     }
 
     function isEnabled(): boolean {
-        return (
-          notebooks.currentWidget !== null &&
-          notebooks.currentWidget === app.shell.currentWidget
-        );
+      return (
+        notebooks.currentWidget !== null &&
+        notebooks.currentWidget === app.shell.currentWidget
+      );
     }
 
     let counter = 0;
@@ -109,64 +105,63 @@ const extension: JupyterLabPlugin<void> = {
     }
 
     function getVoilaUrl(path: string): string {
-        const baseUrl = PageConfig.getBaseUrl();
-        return `${baseUrl}voila/render/${path}`;
+      const baseUrl = PageConfig.getBaseUrl();
+      return `${baseUrl}voila/render/${path}`;
     }
 
     app.commands.addCommand(CommandIDs.voilaRender, {
-        label: 'Render Notebook with Voila',
-        execute: async (args) => {
-          const current = getCurrent(args);
-          if (!current) {
-            return;
-          }
-          const voilaPath = current.context.path;
-          const voilaUrl = getVoilaUrl(voilaPath);
-          const name = PathExt.basename(voilaPath);
-          let widget = voilaIFrame(voilaUrl, name);
-          app.shell.addToMainArea(widget, { mode: 'split-right'});
-          return widget;
-        },
-        isEnabled
+      label: "Render Notebook with Voila",
+      execute: async args => {
+        const current = getCurrent(args);
+        if (!current) {
+          return;
+        }
+        const voilaPath = current.context.path;
+        const voilaUrl = getVoilaUrl(voilaPath);
+        const name = PathExt.basename(voilaPath);
+        let widget = voilaIFrame(voilaUrl, name);
+        app.shell.addToMainArea(widget, { mode: "split-right" });
+        return widget;
+      },
+      isEnabled
     });
 
     app.commands.addCommand(CommandIDs.voilaOpen, {
-        label: 'Open with Voila in New Browser Tab',
-        execute: async (args) => {
-          const current = getCurrent(args);
-          if (!current) {
-            return;
-          }
-          const voilaUrl = getVoilaUrl(current.context.path);
-          window.open(voilaUrl);
-        },
-        isEnabled
+      label: "Open with Voila in New Browser Tab",
+      execute: async args => {
+        const current = getCurrent(args);
+        if (!current) {
+          return;
+        }
+        const voilaUrl = getVoilaUrl(current.context.path);
+        window.open(voilaUrl);
+      },
+      isEnabled
     });
 
     if (palette) {
-      const category = 'Notebook Operations';
-      [
-        CommandIDs.voilaRender,
-        CommandIDs.voilaOpen,
-      ].forEach(command => {
-        palette.addItem({ command, category })
+      const category = "Notebook Operations";
+      [CommandIDs.voilaRender, CommandIDs.voilaOpen].forEach(command => {
+        palette.addItem({ command, category });
       });
     }
 
     if (menu) {
-      menu.viewMenu.addGroup([
-        {
-          command: CommandIDs.voilaRender
-        },
-        {
-          command: CommandIDs.voilaOpen
-        }
-      ], 1000);
+      menu.viewMenu.addGroup(
+        [
+          {
+            command: CommandIDs.voilaRender
+          },
+          {
+            command: CommandIDs.voilaOpen
+          }
+        ],
+        1000
+      );
     }
 
     let voilaButton = new VoilaRenderButton(app);
-    app.docRegistry.addWidgetExtension('Notebook', voilaButton);
-    
+    app.docRegistry.addWidgetExtension("Notebook", voilaButton);
   }
 };
 
