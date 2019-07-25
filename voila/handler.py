2877c516dc3c5dec6979c8af9fd3eb541bf536be
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2019-07-25 16:24:37 +0200

    Convert LazyConfigValue object to dict

--- voila/handler.py
+++ voila/handler.py
@@ -69,7 +69,7 @@ class VoilaHandler(JupyterHandler):
         }
 
         # include potential extra resources
-        extra_resources = self.voila_configuration.config.VoilaConfiguration.resources
+        extra_resources = self.voila_configuration.config.VoilaConfiguration.resources.to_dict()
         if extra_resources:
             recursive_update(resources, extra_resources)
 
