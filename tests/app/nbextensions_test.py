df8c6bf1c66e6beda76de957eac607a0b3022d89
Zsailer <zachsailer@gmail.com>
2019-09-12 11:17:21 -0700

    nbextensions_test passing

---
+++ tests/app/nbextensions_test.py
@@ -0,0 +1,28 @@
+# tests programmatic config of template sytem
+import pytest
+import os
+
+BASE_DIR = os.path.dirname(__file__)
+
+
+@pytest.fixture
+def voila_config():
+    os.environ['JUPYTER_CONFIG_DIR'] = os.path.join(BASE_DIR, '../configs/general')
+    yield {}
+    del os.environ['JUPYTER_CONFIG_DIR']
+
+
+@pytest.fixture
+def voila_config_file_paths_arg():
+    # we don't want the tests to use any configuration on the system
+    path = os.path.abspath(os.path.join(BASE_DIR, '../configs/general'))
+    return '--Voila.config_file_paths=[%r]' % path
+
+
+@pytest.mark.gen_test
+def test_lists_extension(http_client, default_url):
+    response = yield http_client.fetch(default_url)
+    assert response.code == 200
+    html_text = response.body.decode('utf-8')
+    assert 'Hi Voila' in html_text
+    assert 'ipytest/extension.js' in html_text
