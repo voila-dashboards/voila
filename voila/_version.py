18c14de83a82c83aa984e961cffe2f9df74dc1e3
Maarten A. Breddels <maartenbreddels@gmail.com>
2019-09-20 19:57:05 +0200

    Release 0.1.11

--- voila/_version.py
+++ voila/_version.py
@@ -6,5 +6,5 @@
 # The full license is in the file LICENSE, distributed with this software.  #
 #############################################################################
 
-version_info = (0, 1, 10)
+version_info = (0, 1, 11)
 __version__ = '.'.join(map(str, version_info))
