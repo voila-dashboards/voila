d93adaca4acfa809dbaffeaa56eba71f13a2fcd8
Sylvain Corlay <sylvain.corlay@gmail.com>
2019-09-25 14:33:39 +0200

    Update copyright statements before moving to voila-dashboards

--- docs/source/deploy.rst
+++ docs/source/deploy.rst
@@ -1,3 +1,10 @@
+.. Copyright (c) 2018, Voila Contributors
+   Copyright (c) 2018, QuantStack
+
+   Distributed under the terms of the BSD 3-Clause License.
+
+   The full license is in the file LICENSE, distributed with this software.
+
 ===============
 Deploying Voila
 ===============
--- voila/execute.py
+++ voila/execute.py
@@ -1,5 +1,6 @@
 #############################################################################
 # Copyright (c) 2018, Voila Contributors                                    #
+# Copyright (c) 2018, QuantStack                                            #
 #                                                                           #
 # Distributed under the terms of the BSD 3-Clause License.                  #
 #                                                                           #
