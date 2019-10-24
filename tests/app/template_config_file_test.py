8bcce2b3523ab8dd16d60debe801eec3ca10d12a
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2019-10-24 21:11:23 +0200

    Add test for resources used in template system

---
+++ tests/app/template_config_file_test.py
@@ -0,0 +1,22 @@
+# tests config of template sytem from JSON file
+import pytest
+
+import os
+
+BASE_DIR = os.path.dirname(__file__)
+
+
+@pytest.fixture
+def voila_args_extra():
+    path_test_template = os.path.abspath(os.path.join(BASE_DIR, '../test_template/share/jupyter/voila/templates/test_template/nbconvert_templates'))
+    path_default = os.path.abspath(os.path.join(BASE_DIR, '../../share/jupyter/voila/templates/default/nbconvert_templates'))
+    return ['--template=test_template', '--Voila.nbconvert_template_paths=[%r, %r]' % (path_test_template, path_default)]
+
+
+@pytest.mark.gen_test
+def test_template_test(http_client, base_url):
+    response = yield http_client.fetch(base_url)
+    assert response.code == 200
+    assert 'test_template.css' in response.body.decode('utf-8')
+    assert 'Hi Voila' in response.body.decode('utf-8')
+    assert 'default value' in response.body.decode('utf-8')
