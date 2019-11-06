3135205f21415aedee4d0cce4fe1b7628d9a978b
Sylvain Corlay <sylvain.corlay@gmail.com>
2019-11-06 18:14:37 -0500

    move to mathjax3

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -7,11 +7,11 @@ var rules = [
         'css-loader'
     ]},
     // required to load font-awesome
-    { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/font-woff' },
-    { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/font-woff' },
-    { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/octet-stream' },
-    { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, use: 'file-loader' },
-    { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=image/svg+xml' }
+    { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/font-woff&name=/voila/static/[hash].[ext]' },
+    { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/font-woff&name=/voila/static/[hash].[ext]' },
+    { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/octet-stream&name=/voila/static/[hash].[ext]' },
+    { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, use: 'file-loader&name=/voila/static/[hash].[ext]' },
+    { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=image/svg+xml&name=/voila/static/[hash].[ext]' }
 ]
 
 var distRoot = path.resolve(__dirname, '..', 'share', 'jupyter', 'voila', 'templates', 'default', 'static')
