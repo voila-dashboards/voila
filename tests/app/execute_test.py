3cefe672b42e445c8c0ac996fc560fa8a9eb3fd1
Maarten A. Breddels <maartenbreddels@gmail.com>
2019-12-18 10:41:47 +0100

    test: source code was detected because the title matched

--- tests/app/execute_test.py
+++ tests/app/execute_test.py
@@ -19,7 +19,7 @@ def test_hello_world(http_client, base_url):
     assert response.code == 200
     html_text = response.body.decode('utf-8')
     assert 'Hi Voila' in html_text
-    assert 'print' not in html_text, 'by default the source code should be stripped'
+    assert 'print(' not in html_text, 'by default the source code should be stripped'
     assert 'test_template.css' not in html_text, "test_template should not be the default"
 
 
