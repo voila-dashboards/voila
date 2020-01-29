0df907b28c7551c319eb6956c72da7ed5992e62e
martinRenou <martin.renou@gmail.com>
2020-01-29 12:00:46 +0100

    Skip a test on MacOS, Python 3.5
    This test fails due to a broken installation of matplotlib/NumPy. It is
    not related to Voila and passes on Ubuntu, Python 3.5.
    
    Signed-off-by: martinRenou <martin.renou@gmail.com>

--- tests/app/notebooks_test.py
+++ tests/app/notebooks_test.py
@@ -1,4 +1,6 @@
 # simply tests if some notebooks execute
+import sys
+
 import pytest
 
 
@@ -8,6 +10,7 @@ def voila_args(notebook_directory, voila_args_extra):
 
 
 @pytest.mark.gen_test
+@pytest.mark.skipif(sys.version_info < (3, 6), reason="Matplotlib installation issue on MacOS, Python 3.5")
 def test_other_comms(http_client, base_url):
     response = yield http_client.fetch(base_url + '/voila/render/other_comms.ipynb')
     html_text = response.body.decode('utf-8')
