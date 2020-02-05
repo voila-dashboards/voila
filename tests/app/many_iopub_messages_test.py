bd848e96982762c394750ac511232dfaee8cb9d1
Maarten A. Breddels <maartenbreddels@gmail.com>
2020-02-05 21:20:05 +0100

    test: allow long request timeout

--- tests/app/many_iopub_messages_test.py
+++ tests/app/many_iopub_messages_test.py
@@ -1,5 +1,6 @@
 import os
 import pytest
+import tornado.httpclient
 
 
 @pytest.fixture
@@ -10,6 +11,19 @@ def voila_args_extra():
 @pytest.fixture
 def voila_notebook(notebook_directory):
     return os.path.join(notebook_directory, 'many_iopub_messages.ipynb')
+
+
+@pytest.fixture
+def http_client(request, http_server):
+    """Get an asynchronous HTTP client.
+    """
+    client = tornado.httpclient.AsyncHTTPClient(defaults=dict(request_timeout=200))
+
+    def _close():
+        client.close()
+
+    request.addfinalizer(_close)
+    return client
 
 
 @pytest.mark.gen_test
