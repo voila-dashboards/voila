9a4eda0ee3f4e7c45cfecaf3b5f7a0c37debdb32
Maarten A. Breddels <maartenbreddels@gmail.com>
2020-02-05 15:23:09 +0100

    test: expose nbconvert issue, when there are too many iopub messages
    Since nbconvert wait for 1 second at the shell channel, the iopub messages
    pile up, triggering the high water mark of 0mq, causing iopub messages
    to be lost. This results in no output of the cell.

---
+++ tests/app/many_iopub_messages_test.py
@@ -0,0 +1,14 @@
+import os
+import pytest
+
+
+@pytest.fixture
+def voila_notebook(notebook_directory):
+    return os.path.join(notebook_directory, 'many_iopub_messages.ipynb')
+
+
+@pytest.mark.gen_test
+def test_template_cwd(http_client, base_url, notebook_directory):
+    response = yield http_client.fetch(base_url)
+    html_text = response.body.decode('utf-8')
+    assert 'you should see me' in html_text
