5aa8d8f1243c58c93a412767970c51b65269b338
David Brochart <david.brochart@gmail.com>
2020-05-14 10:42:01 +0200

    Use async_start_new_kernel_client from nbclient

--- voila/handler.py
+++ voila/handler.py
@@ -117,7 +117,8 @@ class VoilaHandler(JupyterHandler):
 
     async def _jinja_kernel_start(self):
         assert not self.kernel_started, "kernel was already started"
-        kernel_id = await self.kernel_manager.start_kernel(kernel_name=self.notebook.metadata.kernelspec.name, path=self.cwd)
+        self.executor = VoilaExecutor(self.notebook, km=self.kernel_manager, config=self.traitlet_config)
+        kc, kernel_id = await self.executor.async_start_new_kernel_client(kernel_name=self.notebook.metadata.kernelspec.name, path=self.cwd)
         km = self.kernel_manager.get_kernel(kernel_id)
         # When Voila is launched as an app, its kernel manager's type is AsyncMappingKernelManager, and thus
         # its kernel client's type is AsyncKernelClient.
@@ -126,16 +127,7 @@ class VoilaHandler(JupyterHandler):
         # If it's not done, the kernel manager might not be async, which is not a big deal, but we want the kernel
         # client to be async, so we explicitly configure it for this particular case:
         km.client_class = 'jupyter_client.asynchronous.AsyncKernelClient'
-        self.executor = VoilaExecutor(self.notebook, km=km, config=self.traitlet_config)
         self.executor.kc = km.client()
-        self.executor.kc.start_channels()
-        try:
-            await self.executor.kc.wait_for_ready(timeout=self.executor.startup_timeout)
-        except RuntimeError:
-            self.executor.kc.stop_channels()
-            self.executor.km.shutdown_kernel()
-            raise
-        self.executor.kc.allow_stdin = False
         self.kernel_started = True
         return kernel_id
 
@@ -146,7 +138,7 @@ class VoilaHandler(JupyterHandler):
         # see the updated variable (it seems to be local to our block)
         nb.cells = result.cells
 
-    async def _jinja_cell_generator(self, nb, kernel_id):
+    async def _jinja_cell_generator(self, nb):
         """Generator that will execute a single notebook cell at a time"""
         nb, resources = ClearOutputPreprocessor().preprocess(nb, {'metadata': {'path': self.cwd}})
 
