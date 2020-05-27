cae663463d2e5fbc6529de73cec1033c63a41d83
Sylvain Corlay <sylvain.corlay@gmail.com>
2020-05-27 17:53:32 +0200

    Now that we have dropped Python2, which can use shutils

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-from __future__ import print_function
-
 from setuptools import setup, find_packages, Command
 from setuptools.command.sdist import sdist
 from setuptools.command.build_py import build_py
@@ -12,6 +10,7 @@ from subprocess import check_call, CalledProcessError
 import os
 import shlex
 import sys
+import shutil
 
 from distutils import log
 
@@ -62,62 +61,6 @@ def update_package_data(distribution):
     build_py.finalize_options()
 
 
-# TODO: remove this function once we drop Python2, see:
-#  https://github.com/voila-dashboards/voila/pull/322
-# `shutils.which` function copied verbatim from the Python-3.3 source.
-def which(cmd, mode=os.F_OK | os.X_OK, path=None):
-    """Given a command, mode, and a PATH string, return the path which
-    conforms to the given mode on the PATH, or None if there is no such
-    file.
-    `mode` defaults to os.F_OK | os.X_OK. `path` defaults to the result
-    of os.environ.get("PATH"), or can be overridden with a custom search
-    path.
-    """
-
-    # Check that a given file can be accessed with the correct mode.
-    # Additionally check that `file` is not a directory, as on Windows
-    # directories pass the os.access check.
-    def _access_check(fn, mode):
-        return (os.path.exists(fn) and os.access(fn, mode) and
-                not os.path.isdir(fn))
-
-    # Short circuit. If we're given a full path which matches the mode
-    # and it exists, we're done here.
-    if _access_check(cmd, mode):
-        return cmd
-
-    path = (path or os.environ.get("PATH", os.defpath)).split(os.pathsep)
-
-    if sys.platform == "win32":
-        # The current directory takes precedence on Windows.
-        if os.curdir not in path:
-            path.insert(0, os.curdir)
-
-        # PATHEXT is necessary to check on Windows.
-        pathext = os.environ.get("PATHEXT", "").split(os.pathsep)
-        # See if the given file matches any of the expected path extensions.
-        # This will allow us to short circuit when given "python.exe".
-        matches = [cmd for ext in pathext if cmd.lower().endswith(ext.lower())]
-        # If it does match, only test that one, otherwise we have to try
-        # others.
-        files = [cmd] if matches else [cmd + ext.lower() for ext in pathext]
-    else:
-        # On other platforms you don't have things like PATHEXT to tell you
-        # what file suffixes are executable, so just pass on cmd as-is.
-        files = [cmd]
-
-    seen = set()
-    for dir in path:
-        dir = os.path.normcase(dir)
-        if dir not in seen:
-            seen.add(dir)
-            for thefile in files:
-                name = os.path.join(dir, thefile)
-                if _access_check(name, mode):
-                    return name
-    return None
-
-
 # TODO: remove this function once we can depend on jupyter_packing, see:
 #  https://github.com/voila-dashboards/voila/pull/322
 # `run` function copied from jupyter_packaging under the following license:
@@ -158,7 +101,7 @@ def run(cmd, **kwargs):
     kwargs.setdefault('shell', os.name == 'nt')
     if not isinstance(cmd, (list, tuple)):
         cmd = shlex.split(cmd)
-    cmd_path = which(cmd[0])
+    cmd_path = shutil.which(cmd[0])
     if not cmd_path:
         sys.exit("Aborting. Could not find cmd (%s) in path. "
                  "If command is not expected to be in user's path, "
