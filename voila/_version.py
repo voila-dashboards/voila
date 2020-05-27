b75e05f4041aaaf91b3ee9e3c08848136ebec5a3
Sylvain Corlay <sylvain.corlay@gmail.com>
2020-05-28 00:34:00 +0200

    Flake8

--- voila/_version.py
+++ voila/_version.py
@@ -11,5 +11,5 @@ version_info = (0, 2, 0, 'alpha', 0)
 
 _specifier_ = {'alpha': 'a', 'beta': 'b', 'candidate': 'rc', 'final': ''}
 
-__version__ = '%s.%s.%s%s'%(version_info[0], version_info[1], version_info[2],
-  '' if version_info[3]=='final' else _specifier_[version_info[3]] + str(version_info[4]))
+__version__ = '%s.%s.%s%s' % (version_info[0], version_info[1], version_info[2],
+                              '' if version_info[3] == 'final' else _specifier_[version_info[3]] + str(version_info[4]))
