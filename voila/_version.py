0ecb4183bd7559ef52a61a4c4f4b6f1916c6f2b0
Sylvain Corlay <sylvain.corlay@gmail.com>
2020-06-02 06:51:09 +0200

    Pre-release 0.2.0a1

--- voila/_version.py
+++ voila/_version.py
@@ -7,7 +7,7 @@
 # The full license is in the file LICENSE, distributed with this software.  #
 #############################################################################
 
-version_info = (0, 2, 0, 'alpha', 0)
+version_info = (0, 2, 0, 'alpha', 1)
 
 _specifier_ = {'alpha': 'a', 'beta': 'b', 'candidate': 'rc', 'final': ''}
 
