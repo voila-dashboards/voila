1556ac13595b70c1a403493641a55c72146e1f2e
Carlos Herrero <carlosherrerocontact@gmail.com>
2020-08-14 16:46:25 +0200

    Log js with template

--- share/jupyter/voila/templates/base/static/main.js
+++ share/jupyter/voila/templates/base/static/main.js
@@ -10,37 +10,6 @@
 require(['static/voila'], function(voila) {
     // requirejs doesn't like to be passed an async function, so create one inside
     (async function() {
-
-        const _debug = console.debug;
-        const _info = console.info;
-        const _warn = console.warn;
-        const _error = console.error;
-
-        console.debug = (...args) => {
-            window.top.postMessage({ level: "debug", msg: ["[VOILA]:", ...args] });
-            _debug(...args);
-        };
-
-        console.info = console.info = (...args) => {
-            window.top.postMessage({ level: "info", msg: ["[VOILA]:", ...args] });
-            _info(...args);
-        };
-
-        console.warn = (...args) => {
-            window.top.postMessage({ level: "warn", msg: ["[VOILA]:", ...args] });
-            _warn(...args);
-        };
-
-        console.error = (...args) => {
-            window.top.postMessage({ level: "error", msg: ["[VOILA]:", ...args] });
-            _error(...args);
-        };
-
-        //console.debug("debug");
-        //console.info("info");
-        //console.warn("warn");
-        //console.error("error");
-
         var kernel = await voila.connectKernel()
 
         const context = {
