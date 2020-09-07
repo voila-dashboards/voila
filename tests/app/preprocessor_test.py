7beff160b22e32f1e76660b442b2e0cd1174244f
Maarten A. Breddels <maartenbreddels@gmail.com>
2020-09-07 17:52:47 +0200

    test: preprocessors in conf.json
    Closes #665, which was already working, but now we have a proper test
    setup.
    
    It's also a nice demo on how to use it, and could be referred to in the
    docs.

---
+++ tests/app/preprocessor_test.py
@@ -0,0 +1,21 @@
+# tests the --template argument of Voilà
+import pytest
+import os
+
+
+@pytest.fixture
+def voila_notebook(notebook_directory):
+    return os.path.join(notebook_directory, 'skip-voila-cell.ipynb')
+
+
+@pytest.fixture
+def voila_args_extra():
+    return ['--template=test_template']
+
+
+async def test_markdown_preprocessor(http_server_client, base_url):
+    response = await http_server_client.fetch(base_url)
+    assert response.code == 200
+    html_text = response.body.decode('utf-8')
+    assert 'Hi Voilà cell' in html_text
+    assert 'Hi non Voilà cell' not in html_text
