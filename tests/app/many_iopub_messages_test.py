592777c15405220f3acccf1a5c739487bf91c4a5
Maarten A. Breddels <maartenbreddels@gmail.com>
2020-09-11 13:28:02 +0200

    chore(ci): many_iopub_message test still timed out
    The fix was a fluke, this is the real fix to #686

--- tests/app/many_iopub_messages_test.py
+++ tests/app/many_iopub_messages_test.py
@@ -1,6 +1,5 @@
 import os
 import pytest
-import tornado.httpclient
 
 MAX_TIMEOUT_SECONDS = 240
 
@@ -15,20 +14,7 @@ def voila_notebook(notebook_directory):
     return os.path.join(notebook_directory, 'many_iopub_messages.ipynb')
 
 
-@pytest.fixture
-def http_client(request, http_server):
-    """Get an asynchronous HTTP client.
-    """
-    client = tornado.httpclient.AsyncHTTPClient(defaults=dict(request_timeout=MAX_TIMEOUT_SECONDS+20))
-
-    def _close():
-        client.close()
-
-    request.addfinalizer(_close)
-    return client
-
-
 async def test_template_cwd(http_server_client, base_url, notebook_directory):
-    response = await http_server_client.fetch(base_url)
+    response = await http_server_client.fetch(base_url, request_timeout=MAX_TIMEOUT_SECONDS+20)
     html_text = response.body.decode('utf-8')
     assert 'you should see me' in html_text
