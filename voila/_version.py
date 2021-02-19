14b3d73d85a9eda9e24dceed595991641556a666
Sylvain Corlay <sylvain.corlay@gmail.com>
2021-02-19 23:34:44 +0100

    Release 0.2.7

--- voila/_version.py
+++ voila/_version.py
@@ -7,7 +7,7 @@
 # The full license is in the file LICENSE, distributed with this software.  #
 #############################################################################
 
-version_info = (0, 2, 6, 'final', 0)
+version_info = (0, 2, 7, 'final', 0)
 
 _specifier_ = {'alpha': 'a', 'beta': 'b', 'candidate': 'rc', 'final': ''}
 
