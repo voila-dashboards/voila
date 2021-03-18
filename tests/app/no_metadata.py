8cbd16cc42b313cee91b9d6956baf636acae3b23
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2021-03-18 17:30:41 +0100

    Test notebook without metadata

---
+++ tests/app/no_metadata.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+@pytest.fixture
+def non_existing_notebook_metadata(base_url):
+    return base_url + "voila/render/no_metadata.ipynb"
+
+
+@pytest.fixture
+def voila_args(notebook_directory, voila_args_extra):
+    return ['--VoilaTest.root_dir=%r' % notebook_directory] + voila_args_extra
+
+
+async def test_non_existing_metadata(http_server_client, non_existing_notebook_metadata):
+    response = await http_server_client.fetch(non_existing_notebook_metadata)
+    assert response.code == 200
+    assert 'Executing without notebook metadata' in response.body.decode('utf-8')
