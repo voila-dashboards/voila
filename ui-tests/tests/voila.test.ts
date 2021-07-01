f29567bf208a5c989d6374a430cd0076411b9185
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2021-07-01 08:06:55 +0200

    Add UI Tests (#916)
    * Add UI Tests
    
    * Add CI workflow to run UI tests
    
    * Rename ui-test-output artifact
    
    * Take screenshot of the iframe
    
    * Install with test dependencies
    
    * Add pandas to test dependencies
    
    * Add reference screenshot
    
    * Wait for .jupyter-widgets in the IFrame
    
    * Wait for the MathJax elements
    
    * Update reference screenshot

---
+++ ui-tests/tests/voila.test.ts
@@ -0,0 +1,77 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+
+import { galata, describe, test } from '@jupyterlab/galata';
+
+import * as path from 'path';
+
+jest.setTimeout(100000);
+
+describe('Voila Visual Regression', () => {
+  beforeAll(async () => {
+    await galata.resetUI();
+    galata.context.capturePrefix = 'voila';
+  });
+
+  afterAll(async () => {
+    galata.context.capturePrefix = '';
+  });
+
+  test('Upload files to JupyterLab', async () => {
+    await galata.contents.moveDirectoryToServer(
+      path.resolve(__dirname, './notebooks'),
+      'uploaded'
+    );
+    expect(
+      await galata.contents.fileExists('uploaded/basics.ipynb')
+    ).toBeTruthy();
+  });
+
+  test('Refresh File Browser', async () => {
+    await galata.filebrowser.refresh();
+  });
+
+  test('Open directory uploaded', async () => {
+    await galata.filebrowser.openDirectory('uploaded');
+    expect(
+      await galata.filebrowser.isFileListedInBrowser('basics.ipynb')
+    ).toBeTruthy();
+  });
+
+  test('Open basics.ipynb with the Voila preview', async () => {
+    const notebook = 'basics.ipynb';
+    await galata.notebook.open(notebook);
+    expect(await galata.notebook.isOpen(notebook)).toBeTruthy();
+    await galata.notebook.activate(notebook);
+    expect(await galata.notebook.isActive(notebook)).toBeTruthy();
+
+    const page = galata.context.page;
+    const previewSelector = '.jp-ToolbarButton .voilaRender';
+    const button = await page.$(previewSelector);
+    button.click();
+
+    await galata.notebook.close(true);
+    await galata.toggleSimpleMode(true);
+
+    const iframe = await page.waitForSelector('iframe');
+    const contentFrame = await iframe.contentFrame();
+
+    await contentFrame.waitForSelector('.jupyter-widgets');
+    await contentFrame.waitForLoadState('networkidle');
+    // for the basics notebook we expect to find 2 MathJax element
+    await contentFrame.waitForSelector('#MathJax-Element-1-Frame');
+    await contentFrame.waitForSelector('#MathJax-Element-2-Frame');
+
+    const imageName = 'basics';
+    await galata.capture.screenshot(imageName, iframe);
+    expect(await galata.capture.compareScreenshot(imageName)).toBe('same');
+  });
+
+  test('Open home directory', async () => {
+    await galata.filebrowser.openHomeDirectory();
+  });
+
+  test('Delete uploaded directory', async () => {
+    await galata.contents.deleteDirectory('uploaded');
+  });
+});
