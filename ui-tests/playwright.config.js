48b817f9490c87bd13d85e014fe107e494f506ca
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2021-09-03 17:23:20 +0200

    Update config

--- ui-tests/playwright.config.js
+++ ui-tests/playwright.config.js
@@ -5,8 +5,6 @@ module.exports = {
   use: {
     baseURL: 'http://localhost:8888/voila/'
   },
-  // Switch to 'always' to keep raw assets for all tests
-  preserveOutput: 'failures-only', // Breaks HTML report if use.video == 'on'
   // Try one retry as some tests are flaky
   retries: 1
 };
