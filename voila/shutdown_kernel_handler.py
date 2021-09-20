09166cd6f9acc4ba8fb59570738d4765ccc87624
Trung Le <leductrungxf@gmail.com>
2021-09-20 23:02:04 +0200

    Add handler to shut down kernel on page's `beforeunload` event

---
+++ voila/shutdown_kernel_handler.py
@@ -0,0 +1,13 @@
+import tornado
+from jupyter_server.base.handlers import APIHandler
+
+
+class VoilaShutdownKernelHandler(APIHandler):
+    """ Handler to shutdown kernel on page's `beforeunload` event.
+    """
+
+    @tornado.web.authenticated
+    async def post(self, kernel_id):
+        await self.kernel_manager.shutdown_kernel(kernel_id)
+        self.set_status(204)
+        self.finish()
