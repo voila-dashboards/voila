06d1d11bd76edda2891e374d0e64dfece7209f26
Duc Trung LE <leductrungxf@gmail.com>
2021-10-04 19:33:12 +0200

    Update many_iopub_messages test

--- tests/app/many_iopub_messages_test.py
+++ tests/app/many_iopub_messages_test.py
@@ -19,8 +19,7 @@ def voila_notebook(notebook_directory):
     return os.path.join(notebook_directory, 'many_iopub_messages.ipynb')
 
 
-async def test_template_cwd(http_server_client, base_url, wait_for_kernel):
-    await wait_for_kernel(10)
+async def test_template_cwd(http_server_client, base_url):
     response = await http_server_client.fetch(base_url, request_timeout=MAX_TIMEOUT_SECONDS+20)
     html_text = response.body.decode('utf-8')
     assert 'you should see me' in html_text
