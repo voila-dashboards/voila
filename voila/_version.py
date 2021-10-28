41d4ba97db594eaf49e73bc2a729b3f82262bd74
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2021-10-28 08:16:45 +0000

    Publish 0.3.0b0
    SHA256 hashes:
    
    voila-0.3.0b0-py3-none-any.whl: bd335002261a5748d7f6d6beefcd0b2f3a62bba449e7736d69737abbaff96f58
    
    voila-0.3.0b0.tar.gz: c3f6144d442558ac82c10dda1937cb3ee47bf8d04d59175deff5767dac02f5d9
    
    voila-dashboards-jupyterlab-preview-2.1.0-beta.0.tgz: 8d1d40b3d88b1e8e48f7beee9ba21735c90f6b726985662807dd278ba55f819a

--- voila/_version.py
+++ voila/_version.py
@@ -17,7 +17,7 @@ VersionInfo = namedtuple(
 )
 
 # DO NOT EDIT THIS DIRECTLY!  It is managed by bumpversion
-version_info = VersionInfo(0, 3, 0, "alpha", 2)
+version_info = VersionInfo(0, 3, 0, "beta", 0)
 
 _specifier_ = {"alpha": "a", "beta": "b", "candidate": "rc", "final": ""}
 
