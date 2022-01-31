b50b123e509a12ff7390bf0afa6c313ee6541d11
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2022-01-31 14:56:28 +0100

    Push new `stable` when not in dry run

--- scripts/reset-stable.sh
+++ scripts/reset-stable.sh
@@ -1,8 +1,9 @@
 set -eux
 
+git checkout stable
+git reset --hard origin/main
+
 # Update the stable branch to point the latest release
 if [[ ${RH_DRY_RUN:=true} != 'true' ]]; then
-    git checkout stable
-    git reset --hard origin/main
     git push origin stable -f
 fi
