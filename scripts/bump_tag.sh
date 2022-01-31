f3f83a7ed8618bc8401eb13f7f5d3e84f6b5254e
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2022-01-31 14:18:32 +0100

    Add script to the bump the `stable` tag on release

---
+++ scripts/bump_tag.sh
@@ -0,0 +1,8 @@
+set -eux
+
+# Update the stable tag to point the latest release
+if [[ ${RH_DRY_RUN:=true} != 'true' ]]; then
+    git tag -f -a stable -m "Github Action release"
+    git push origin -f --tags
+fi
+diff --git a/pyproject.toml b/pyproject.toml
