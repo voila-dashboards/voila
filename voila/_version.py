71292e4124b1f4a6f91c8b4e16ea9ad6b5ef500b
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2022-10-04 09:00:24 +0000

    Publish 0.4.0a2
    SHA256 hashes:
    
    voila-0.4.0a2-py3-none-any.whl: 0362b46982b44c643dd01aef7f9ac74d798929d50b8976f6952d0598046629fb
    
    voila-0.4.0a2.tar.gz: 459eef651c726b186635aa0444a1c9661187889593a8ab6816c8b2360e0485b6
    
    voila-dashboards-jupyterlab-preview-2.2.0-alpha.2.tgz: 5ec5cfa792cb59e760c8efef69b6ce5b395beef07d2a7d7799c8dd0cc349d54a

--- voila/_version.py
+++ voila/_version.py
@@ -15,7 +15,7 @@ import re
 from collections import namedtuple
 
 # Use "hatch version xx.yy.zz" to handle version changes
-__version__ = "0.4.0a1"
+__version__ = "0.4.0a2"
 
 # PEP440 version parser
 _version_regex = re.compile(
