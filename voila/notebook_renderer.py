c0b80ab3d3efea0335cede71751f98cea2edcf91
martinRenou <martin.renou@gmail.com>
2023-06-22 16:41:05 +0000

    Backport PR #1334: Do not clean up resources after execution

--- voila/notebook_renderer.py
+++ voila/notebook_renderer.py
@@ -251,8 +251,6 @@ class NotebookRenderer(LoggingConfigurable):
         #  (it seems to be local to our block)
         nb.cells = result.cells
 
-        await self._cleanup_resources()
-
     async def _jinja_cell_generator(self, nb, kernel_id):
         """Generator that will execute a single notebook cell at a time"""
         nb, _ = ClearOutputPreprocessor().preprocess(
@@ -301,12 +299,6 @@ class NotebookRenderer(LoggingConfigurable):
                     ]
             finally:
                 yield output_cell
-
-        await self._cleanup_resources()
-
-    async def _cleanup_resources(self):
-        await ensure_async(self.executor.km.cleanup_resources())
-        await ensure_async(self.executor.kc.stop_channels())
 
     async def load_notebook(self, path):
 
