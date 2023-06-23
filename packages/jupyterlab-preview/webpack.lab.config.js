b7e478c157db56d3f9342a5d25e302ad24cb9dd4
martinRenou <martin.renou@gmail.com>
2023-06-23 09:50:35 +0200

    Linter

--- packages/jupyterlab-preview/webpack.lab.config.js
+++ packages/jupyterlab-preview/webpack.lab.config.js
@@ -2,5 +2,5 @@ const crypto = require('crypto');
 
 // Workaround for loaders using "md4" by default, which is not supported in FIPS-compliant OpenSSL
 const cryptoOrigCreateHash = crypto.createHash;
-crypto.createHash = (algorithm) =>
-  cryptoOrigCreateHash(algorithm == 'md4' ? 'sha256' : algorithm);
+crypto.createHash = algorithm =>
+  cryptoOrigCreateHash(algorithm === 'md4' ? 'sha256' : algorithm);
