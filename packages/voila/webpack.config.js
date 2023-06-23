bde9f539aea0ae8557942c978ebda774607accd1
martinRenou <martin.renou@gmail.com>
2023-06-23 10:00:33 +0200

    Iterate

--- packages/voila/webpack.config.js
+++ packages/voila/webpack.config.js
@@ -4,8 +4,8 @@ const crypto = require('crypto');
 
 // Workaround for loaders using "md4" by default, which is not supported in FIPS-compliant OpenSSL
 const cryptoOrigCreateHash = crypto.createHash;
-crypto.createHash = (algorithm) =>
-  cryptoOrigCreateHash(algorithm == 'md4' ? 'sha256' : algorithm);
+crypto.createHash = algorithm =>
+  cryptoOrigCreateHash(algorithm === 'md4' ? 'sha256' : algorithm);
 
 var rules = [
   { test: /\.css$/, use: ['style-loader', 'css-loader'] },
